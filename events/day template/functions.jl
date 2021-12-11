f43e44fcca00e5bbb54ed7c014a2b9549e54a945
ShaoWei Teo <tookunokaze@gmail.com>
2021-12-12 01:02:05 +0800

    ✨ add a simple day template

---
+++ events/day template/functions.jl
@@ -0,0 +1,47 @@
+using Chain
+using Combinatorics
+using DataStructures
+using OffsetArrays
+using Mods
+
+
+## Helpers
+
+⋆(f::Base.Callable) = Base.splat(f)
+
+CI = CartesianIndex
+CIS = CartesianIndices
+
+
+## Parse input
+
+function parse_input(filename::String)
+    @chain filename begin
+        readlines
+        @. parse_line
+    end
+end
+
+function parse_line(line)
+    @chain line begin
+        line
+    end
+end
+
+
+## Part 1
+
+function result1(pd)
+    @chain pd begin
+        pd
+    end
+end
+
+
+## Part 2
+
+function result2(pd)
+    @chain pd begin
+        pd
+    end
+end
