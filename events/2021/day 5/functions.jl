0d4e375f25ddf52c7abe198982ce99460128f402
ShaoWei Teo <tookunokaze@gmail.com>
2021-12-13 00:43:17 +0800

    ✨ solve AoC 2021 day 5

---
+++ events/2021/day 5/functions.jl
@@ -0,0 +1,103 @@
+using Chain
+using OffsetArrays
+
+
+## Helpers
+
+CI = CartesianIndex
+CIS = CartesianIndices
+
+Base.show(io::IO, ::MIME"text/plain", c::Char) = print(io, string(c))
+
+
+## Parse input
+
+function parse_input(filename::String)
+    @chain filename begin
+        readlines
+        @. parse_line
+    end
+end
+
+function parse_line(line)
+    @chain line begin
+        split(" -> ")
+        Pair(parse_coor(_[1]), parse_coor(_[2]))
+    end
+end
+
+function parse_coor(s)
+    @chain s begin
+        split(',')
+        parse.(Int, _)
+        CI(_...)
+    end
+end
+
+
+## Part 1
+
+function init_area(lines)
+    return zeros(
+        Int,
+        (
+            range(
+                start = 0,
+                stop = maximum(max(c1[1], c2[1]) for (c1, c2) in lines),
+            ),
+            range(
+                start = 0,
+                stop = maximum(max(c1[2], c2[2]) for (c1, c2) in lines),
+            ),
+        ),
+    )
+end
+
+function is_straight(line)
+    (c1, c2) = line
+    c1[1] == c2[1] || c1[2] == c2[2]
+end
+
+function draw_straight_vents(area, line)
+    (c1, c2) = line
+    vent_indices = collect(c1:c2) ∪ collect(c2:c1)
+    area[vent_indices] .+= 1
+    return area
+end
+
+function result1(lines)
+    area = init_area(lines)
+    for line in lines
+        !is_straight(line) && continue
+        area = draw_straight_vents(area, line)
+    end
+    return count(>(1), area)
+end
+
+
+## Part 2
+
+function is_diagonal(line)
+    !is_straight(line)
+end
+
+function draw_diagonal_vents(area, line)
+    (c1, c2) = line
+    diff_c = c2 - c1
+    step = CI(sign(diff_c[1]), sign(diff_c[2]))
+    width = abs(diff_c[1])
+    area[[c1 + i * step for i in 0:width]] .+= 1
+    return area
+end
+
+function result2(lines)
+    area = init_area(lines)
+    for line in lines
+        if is_straight(line)
+            area = draw_straight_vents(area, line)
+        else
+            area = draw_diagonal_vents(area, line)
+        end
+    end
+    return count(>(1), area)
+end
