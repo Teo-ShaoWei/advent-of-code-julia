d5936dedc80b2a18dd59867fbad20a4c924bc892
ShaoWei Teo <tookunokaze@gmail.com>
2021-12-12 19:38:35 +0800

    ✨ solve AoC 2021 day 2

---
+++ events/2021/day 2/functions.jl
@@ -0,0 +1,85 @@
+using Chain
+using Combinatorics
+using DataStructures
+using OffsetArrays
+using Mods
+
+
+## Helpers
+
+CI = CartesianIndex
+CIS = CartesianIndices
+
+Base.show(io::IO, ::MIME"text/plain", c::Char) = print(io, string(c))
+
+
+## Parse input
+
+function parse_input(filename::String)
+    @chain filename begin
+        readlines
+        @. parse_line
+    end
+end
+
+function parse_line(line)
+    @chain line begin
+        split(' ')
+        (d = _[1], m = parse(Int, _[2]))
+    end
+end
+
+
+## Part 1
+
+function get_move(command)
+    direction = Dict(
+        "forward" => CI(0, 1),
+        "down" => CI(1, 0),
+        "up" => CI(-1, 0),
+    )
+
+    return command.m * direction[command.d]
+end
+
+function result1(commands)
+    @chain commands begin
+        @. get_move
+        sum
+        _[1] * _[2]
+    end
+end
+
+
+## Part 2
+
+function get_move_with_aim(command, aim)
+    direcion_with_aim = Dict(
+        "forward" => CI(0, 1),
+        "down" => CI(1, 0),
+        "up" => CI(-1, 0),
+    )
+
+    if command.d == "down"
+        aim += command.m
+        move = CI(0, 0)
+    elseif command.d == "up"
+        aim -= command.m
+        move = CI(0, 0)
+    else  # command.d == "forward"
+        move = CI(aim * command.m, command.m)
+    end
+
+    return (; move, aim)
+end
+
+function result2(commands)
+    aim = 0
+    pos = CI(0, 0)
+    for command in commands
+        (; move, aim) = get_move_with_aim(command, aim)
+        pos += move
+    end
+
+    return pos[1] * pos[2]
+end
