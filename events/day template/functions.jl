1f733c89ab5519f2971a13474bd59234492302f5
ShaoWei Teo <tookunokaze@gmail.com>
2021-12-25 20:31:23 +0800

    🎨 update template to use `pd_str`
    This allow us to easily read ad-hoc sample data in as puzzle input.

--- events/day template/functions.jl
+++ events/day template/functions.jl
@@ -13,20 +13,37 @@ CIS = CartesianIndices
 Base.show(io::IO, ::MIME"text/plain", c::CI) = print(io, "CI(", join(string.(Tuple(c)), ", "), ")")
 Base.show(io::IO, c::CI) = show(io, "text/plain", c)
 
+Base.show(io::IO, ::MIME"text/plain", c::CIS) = print(io, "CIS((", join(c.indices, ", "), "))")
+Base.show(io::IO, c::CIS) = show(io, "text/plain", c)
+
 Base.show(io::IO, ::MIME"text/plain", c::Char) = print(io, string(c))
 
 
 ## Parse input
 
-function parse_input(filename::String)
+function parse_puzzle_file(filename::String)
     @chain filename begin
-        readlines
-        @. parse_line
+        readchomp
+        parse_puzzle_data
     end
 end
 
-function parse_line(line)
-    @chain line begin
+macro pd_str(s::String)
+    @chain s begin
+        chomp
+        parse_puzzle_data
+    end
+end
+
+function parse_puzzle_data(s)
+    @chain s begin
+        split("\n")
+        @. parse_puzzle_line
+    end
+end
+
+function parse_puzzle_line(s)
+    @chain s begin
         _
     end
 end
