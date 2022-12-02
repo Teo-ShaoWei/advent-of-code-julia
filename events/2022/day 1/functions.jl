dec7a335be105b708b854335c79238c32f7fffc8
ShaoWei Teo <tookunokaze@gmail.com>
2022-12-02 09:35:09 +0800

    solve AoC 2022 day 1

---
+++ events/2022/day 1/functions.jl
@@ -0,0 +1,71 @@
+using Chain
+using Combinatorics
+using DataStructures
+using OffsetArrays
+using Mods
+
+
+## Helpers
+
+CI = CartesianIndex
+CIS = CartesianIndices
+
+Base.show(io::IO, ::MIME"text/plain", c::CI) = print(io, "CI(", join(string.(Tuple(c)), ", "), ")")
+Base.show(io::IO, c::CI) = show(io, "text/plain", c)
+
+Base.show(io::IO, ::MIME"text/plain", c::CIS) = print(io, "CIS((", join(c.indices, ", "), "))")
+Base.show(io::IO, c::CIS) = show(io, "text/plain", c)
+
+Base.show(io::IO, ::MIME"text/plain", c::Char) = print(io, string(c))
+
+
+## Parse input
+
+function parse_puzzle_file(filename::String)
+    @chain filename begin
+        readchomp
+        parse_puzzle_data
+    end
+end
+
+macro pd_str(s::String)
+    @chain s begin
+        chomp
+        parse_puzzle_data
+    end
+end
+
+function parse_puzzle_data(s)
+    @chain s begin
+        split("\n\n")
+        @. parse_elf
+    end
+end
+
+function parse_elf(s)
+    @chain s begin
+        split("\n")
+        parse.(Int, _)
+    end
+end
+
+
+## Part 1
+
+function result1(pd)
+    @chain pd begin
+        @. sum
+        maximum
+    end
+end
+
+
+## Part 2
+
+function result2(pd)
+    @chain pd begin
+        @. sum
+        sort(; rev = true)
+        sum(_[1:3])
+    end
+end
