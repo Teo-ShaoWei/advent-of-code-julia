ca2f294495d10c37cb1f09254ed26211b332624e
ShaoWei Teo <tookunokaze@gmail.com>
2022-12-06 13:14:06 +0800

    solve AoC 2022 day 6

---
+++ events/2022/day 6/functions.jl
@@ -0,0 +1,70 @@
+using Chain
+using Combinatorics
+using DataStructures
+using OffsetArrays
+using Mods
+
+
+## Helpers
+
+CI = CartesianIndex
+CIS = CartesianIndices
+
+Base.show(io::IO, ::MIME"text/plain", c::CI) = print(io, "CI(", join(string.(Tuple(c)), ", "), ")")
+Base.show(io::IO, c::CI) = show(io, "text/plain", c)
+
+Base.show(io::IO, ::MIME"text/plain", c::CIS) = print(io, "CIS((", join(c.indices, ", "), "))")
+Base.show(io::IO, c::CIS) = show(io, "text/plain", c)
+
+Base.show(io::IO, ::MIME"text/plain", c::Char) = print(io, string(c))
+
+
+## Parse input
+
+function parse_puzzle_file(filename::String)
+    @chain filename begin
+        readchomp
+        parse_puzzle_data
+    end
+end
+
+macro pd_str(s::String)
+    @chain s begin
+        chomp
+        parse_puzzle_data
+    end
+end
+
+function parse_puzzle_data(s)
+    @chain s begin
+        split("\n")
+        @. parse_puzzle_line
+        only
+    end
+end
+
+function parse_puzzle_line(s)
+    @chain s begin
+        string
+    end
+end
+
+function first_consecutive(msg, n)
+    for i in n:length(msg)
+        if length(Set(msg[(i - n + 1):i])) == n
+            return i
+        end
+    end
+end
+
+## Part 1
+
+function result1(pd)
+    first_consecutive(pd, 4)
+end
+
+## Part 2
+
+function result2(pd)
+    first_consecutive(pd, 14)
+end
