6e653052412af2417e704f11705d1473e47388cd
ShaoWei Teo <tookunokaze@gmail.com>
2023-12-01 20:54:54 +0800

    update solution to AoC 2015 day 1

--- events/2015/day 1/functions.jl
+++ events/2015/day 1/functions.jl
@@ -1,40 +1,70 @@
 using Chain
+using Combinatorics
+using DataStructures
+using OffsetArrays
+using Mods
 
 
 ## Helpers
 
-CI = CartesianIndex
-CIS = CartesianIndices
+import AdventOfCode:
+    AdventOfCode,
+    CI, CIS,
+    make_smallest_boundary,
+    parse_matrix,
+    print_matrix
+
+Base.show(io::IO, ::MIME"text/plain", c::CI) = print(io, "CI(", join(string.(Tuple(c)), ", "), ")")
+Base.show(io::IO, c::CI) = show(io, "text/plain", c)
+
+Base.show(io::IO, ::MIME"text/plain", c::CIS) = print(io, "CIS((", join(c.indices, ", "), "))")
+Base.show(io::IO, c::CIS) = show(io, "text/plain", c)
 
 Base.show(io::IO, ::MIME"text/plain", c::Char) = print(io, string(c))
+
+Base.show(io::IO, ::MIME"text/plain", v::Base.Iterators.Enumerate) = foreach((x,) -> println(io, "{ ", x[1], ": ", x[2], " }"), v)
+Base.show(io::IO, v::Base.Iterators.Enumerate) = show(io, "text/plain", v)
+
+# Base.show(io::IO, ::MIME"text/plain", v::Vector) = print(io, "[", join(v, ", "), "]")
+# Base.show(io::IO, ::MIME"text/plain", v::Vector{Vector{T}} where {T}) = show(io, enumerate(v))
+Base.show(io::IO, v::Vector) = print(io, "[", join(v, ", "), "]")
 
 
 ## Parse input
 
-function parse_input(filename::String)
+function parse_puzzle_file(filename::String)
     @chain filename begin
-        readlines
-        only
-        parse_line
+        readchomp
+        parse_puzzle_data
     end
 end
 
-function parse_line(line)
-    @chain line begin
-        split("")
-        @. only
+macro pd_str(s::String)
+    @chain s begin
+        chomp
+        parse_puzzle_data
     end
+end
+
+function parse_puzzle_data(s)
+    @chain s begin
+        split("")
+        @. parse_char
+    end
+end
+
+function parse_char(s)
+    return only(s)
 end
 
 
 ## Part 1
 
 function get_move(c)
-    move = Dict(
+    Dict(
         '(' => 1,
         ')' => -1,
-    )
-    move[c]
+    )[c]
 end
 
 function result1(pd)
