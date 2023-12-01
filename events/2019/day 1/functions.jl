780dae26594c60e7d93ab60a8b3d302ad7ce74fb
ShaoWei Teo <tookunokaze@gmail.com>
2023-12-02 02:53:12 +0800

    solve AoC 2019 day 1

---
+++ events/2019/day 1/functions.jl
@@ -0,0 +1,87 @@
+using Chain
+using Combinatorics
+using DataStructures
+using OffsetArrays
+using Mods
+
+
+## Helpers
+
+import AdventOfCode:
+    AdventOfCode,
+    CI, CIS,
+    make_smallest_boundary,
+    parse_matrix,
+    print_matrix
+
+Base.show(io::IO, ::MIME"text/plain", c::CI) = print(io, "CI(", join(string.(Tuple(c)), ", "), ")")
+Base.show(io::IO, c::CI) = show(io, "text/plain", c)
+
+Base.show(io::IO, ::MIME"text/plain", c::CIS) = print(io, "CIS((", join(c.indices, ", "), "))")
+Base.show(io::IO, c::CIS) = show(io, "text/plain", c)
+
+Base.show(io::IO, ::MIME"text/plain", c::Char) = print(io, string(c))
+
+# Base.show(io::IO, ::MIME"text/plain", v::Vector) = print(io, "[", join(v, ", "), "]")
+Base.show(io::IO, v::Vector) = print(io, "[", join(v, ", "), "]")
+
+
+## Parse input
+
+function parse_puzzle_file(filename::String)
+    @chain filename begin
+        readchomp
+        parse_puzzle_data
+    end
+end
+
+macro pd_str(s::String)
+    @chain s begin
+        chomp
+        parse_puzzle_data
+    end
+end
+
+function parse_puzzle_data(s)
+    @chain s begin
+        split("\n")
+        @. parse_puzzle_line
+    end
+end
+
+function parse_puzzle_line(s)
+    parse(Int, s)
+end
+
+
+## Part 1
+
+function get_fuel(mass)
+    mass ÷ 3 - 2
+end
+
+function result1(pd::Vector{Int})
+    @chain pd begin
+        @. get_fuel
+        sum
+    end
+end
+
+
+## Part 2
+
+function get_total_fuel(mass)
+    fuel = 0
+    while mass > 5
+        mass = get_fuel(mass)
+        fuel += mass
+    end
+    return fuel
+end
+
+function result2(pd)
+    @chain pd begin
+        @. get_total_fuel
+        sum
+    end
+end
