02fc40619d0109c69386b84942442987878b0b40
ShaoWei Teo <tookunokaze@gmail.com>
2023-12-02 01:13:16 +0800

    update solution to AoC 2015 day 2

--- events/2015/day 2/functions.jl
+++ events/2015/day 2/functions.jl
@@ -1,54 +1,79 @@
 using Chain
+using Combinatorics
+using DataStructures
+using OffsetArrays
+using Mods
 
 
 ## Helpers
 
-CI = CartesianIndex
-CIS = CartesianIndices
+import AdventOfCode:
+    AdventOfCode,
+    CI, CIS,
+    make_smallest_boundary,
+    parse_matrix,
+    print_matrix
+
+Base.show(io::IO, ::MIME"text/plain", c::CI) = print(io, "CI(", join(string.(Tuple(c)), ", "), ")")
+Base.show(io::IO, c::CI) = show(io, "text/plain", c)
+
+Base.show(io::IO, ::MIME"text/plain", c::CIS) = print(io, "CIS((", join(c.indices, ", "), "))")
+Base.show(io::IO, c::CIS) = show(io, "text/plain", c)
 
 Base.show(io::IO, ::MIME"text/plain", c::Char) = print(io, string(c))
+
+# Base.show(io::IO, ::MIME"text/plain", v::Vector) = print(io, "[", join(v, ", "), "]")
+Base.show(io::IO, v::Vector) = print(io, "[", join(v, ", "), "]")
 
 
 ## Parse input
 
-function parse_input(filename::String)
+function parse_puzzle_file(filename::String)
     @chain filename begin
-        readlines
-        @. parse_line
+        readchomp
+        parse_puzzle_data
     end
 end
 
-function parse_line(line)
-    @chain line begin
-        match(r"(\d+)x(\d+)x(\d+)", _)
-        collect
+macro pd_str(s::String)
+    @chain s begin
+        chomp
+        parse_puzzle_data
+    end
+end
+
+function parse_puzzle_data(s)
+    @chain s begin
+        split("\n")
+        @. parse_puzzle_line
+    end
+end
+
+function parse_puzzle_line(s)
+    @chain s begin
+        split("x")
         parse.(Int, _)
-        (; ((:l, :w, :h) .=> _)...)
     end
 end
 
 
 ## Part 1
 
-function smallest_side_dims(dims)
+function get_slack_area(dims)
     @chain dims begin
-        collect
         sort!
-        Iterators.take(2)
-        collect
+        _[1: 2]
+        prod
     end
 end
 
-function get_wrapping_paper_size(dims)
-    (; l, w, h) = dims
-    surface = 2*l*w + 2*w*h + 2*h*l
-    slack = smallest_side_dims(dims) |> prod
-    return surface + slack
+function get_surface_area((l, w, h))
+    2 * l * w + 2 * w * h + 2 * h * l
 end
 
-function result1(dimss)
-    @chain dimss begin
-        @. get_wrapping_paper_size
+function result1(pd)
+    @chain pd begin
+        [get_slack_area(dims) + get_surface_area(dims) for dims in _]
         sum
     end
 end
@@ -56,15 +81,22 @@ end
 
 ## Part 2
 
-function get_ribbon_length(dims)
-    wrap = 2 * sum(smallest_side_dims(dims))
-    ribbon = prod(dims)
-    return wrap + ribbon
+function get_ribbon_wrap(dims)
+    @chain dims begin
+        sort!
+        _[1:2]
+        sum
+        _ * 2
+    end
 end
 
-function result2(dimss)
-    @chain dimss begin
-        @. get_ribbon_length
+function get_ribbon_bow(dims)
+    prod(dims)
+end
+
+function result2(pd)
+    @chain pd begin
+        [get_ribbon_wrap(dims) + get_ribbon_bow(dims) for dims in _]
         sum
     end
 end
