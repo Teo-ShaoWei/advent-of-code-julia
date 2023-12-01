576c93f41f5657e7296bb2548af54140c398eb7f
ShaoWei Teo <tookunokaze@gmail.com>
2023-12-02 02:23:38 +0800

    solve AoC 2018 day 1

---
+++ events/2018/day 1/functions.jl
@@ -0,0 +1,74 @@
+using Chain
+using Combinatorics
+using DataStructures
+using OffsetArrays
+using Mods
+
+
+## Helpers
+
+import AdventOfCode:
+    AdventOfCode,
+    CI, CIS,
+    make_smallest_boundary,
+    parse_matrix,
+    print_matrix
+
+Base.show(io::IO, ::MIME"text/plain", c::CI) = print(io, "CI(", join(string.(Tuple(c)), ", "), ")")
+Base.show(io::IO, c::CI) = show(io, "text/plain", c)
+
+Base.show(io::IO, ::MIME"text/plain", c::CIS) = print(io, "CIS((", join(c.indices, ", "), "))")
+Base.show(io::IO, c::CIS) = show(io, "text/plain", c)
+
+Base.show(io::IO, ::MIME"text/plain", c::Char) = print(io, string(c))
+
+# Base.show(io::IO, ::MIME"text/plain", v::Vector) = print(io, "[", join(v, ", "), "]")
+Base.show(io::IO, v::Vector) = print(io, "[", join(v, ", "), "]")
+
+
+## Parse input
+
+function parse_puzzle_file(filename::String)
+    @chain filename begin
+        readchomp
+        parse_puzzle_data
+    end
+end
+
+macro pd_str(s::String)
+    @chain s begin
+        chomp
+        parse_puzzle_data
+    end
+end
+
+function parse_puzzle_data(s)
+    @chain s begin
+        split("\n")
+        @. parse_puzzle_line
+    end
+end
+
+function parse_puzzle_line(s)
+    Meta.parse.(s)
+end
+
+
+## Part 1
+
+function result1(pd)
+    sum.(pd)
+end
+
+
+## Part 2
+
+function result2(pd)
+    current = 0
+    seen = Set(current)
+    for x in Iterators.cycle(pd)
+        current += x
+        (current ∈ seen) && return current
+        push!(seen, current)
+    end
+end
