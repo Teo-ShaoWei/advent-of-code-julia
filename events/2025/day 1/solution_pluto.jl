a1dae841fb736e2b058b5dc43ca6629b29827f34
ShaoWei Teo <tookunokaze@gmail.com>
2025-12-01 14:23:18 +0800

    feat: solve AoC 2025 day 1

---
+++ events/2025/day 1/solution_pluto.jl
@@ -0,0 +1,286 @@
+### A Pluto.jl notebook ###
+# v0.20.21
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 090b4c81-9cda-42e4-a9ff-22fbf5844fa0
+begin
+	using Pkg
+	Pkg.activate(Base.current_project())
+end
+
+# ╔═╡ 90815a37-6e8e-4c01-9e22-52dd96ab457b
+using PlutoUI
+
+# ╔═╡ 56736de4-cc48-11f0-a6b8-e199bea039a2
+begin
+	using Chain
+	using Combinatorics
+	using DataStructures
+	using OffsetArrays
+	using Mods
+	using UnPack
+end
+
+# ╔═╡ 213c3a51-ce8d-4c6f-811e-7f551fd2c356
+begin
+	matched = match(r"events/(?<year>\d+)/day (?<day>\d+)$", pwd())
+	if isnothing(matched)
+		md"""
+		# Advent of Code template
+		Copy this to an AoC event (`events/<year>/day <day>/`) as the starting point for the puzzle.
+		"""
+	else
+		@unpack year, day = NamedTuple(matched)
+		problem_link = "[Problem link](https://adventofcode.com/$(year)/day/$(day))"
+	
+		Markdown.parse(
+			"""
+			# Advent of Code $(year) Day $(day)
+			$(problem_link)
+			"""
+		)
+	end
+end
+
+# ╔═╡ bfd79601-6001-4737-bc9f-f13e02150c3c
+md"""
+## Parse puzzle input
+"""
+
+# ╔═╡ afe15c3c-0a5f-4db4-8f72-bd22b0b7e27b
+function parse_puzzle_line(s)
+    @chain s begin
+        match(r"(?<dir>.)(?<mag>\d+)", _)
+		NamedTuple
+		(; dir = Symbol(_[1]), mag = parse(Int, _[2]))
+    end
+end
+
+# ╔═╡ da7e182a-6c9d-4c43-91d4-b32be2545718
+function parse_puzzle_data(s::String)
+    @chain s begin
+        split("\n")
+		@. string
+        @. parse_puzzle_line
+    end
+end
+
+# ╔═╡ ccd4eb29-5eb4-469c-99d7-1cdcd679fb66
+function parse_puzzle_file(filename::String)
+    @chain filename begin
+        readchomp
+		string
+        parse_puzzle_data
+    end
+end
+
+# ╔═╡ d2638404-b96d-4362-850f-98fd5dabd5be
+macro pd_str(s::String)
+    @chain s begin
+        chomp
+		string
+        parse_puzzle_data
+    end
+end
+
+# ╔═╡ d97c6185-39c1-4d26-b2d2-66dd000c317b
+PDI = parse_puzzle_file("input.txt")
+
+# ╔═╡ 5436ac6a-5869-4b8d-9b9e-56e731589cbd
+md"""
+## Part 1
+"""
+
+# ╔═╡ e9fc4134-09f9-43b8-8066-24ab50a2f64c
+# puzzle part 1 data samples
+PDS_part1 = [
+	parse_puzzle_file("sample1.txt"),
+]
+
+# ╔═╡ 4c4f78ee-b679-4597-9a0e-a01a2060f231
+function result1(pd)
+	x = Mod{100}(50)
+	zero_counter = 0
+    for (; dir, mag) in pd
+		x = (dir == :R) ? x + mag : x - mag
+		(x == 0) && (zero_counter += 1)
+	end
+	return zero_counter
+end
+
+# ╔═╡ 5bdfe21f-9535-47f3-b162-5c63e49022ac
+result1.(PDS_part1)
+
+# ╔═╡ 451ff7c6-1e2f-4717-9c15-92760473609c
+md"""
+### answer
+"""
+
+# ╔═╡ 0afa794b-17ef-4e24-a5de-caf2d4ff966e
+@time @info(
+    "part 1 answer",
+    result1(PDI),
+)
+
+# ╔═╡ 2bbaf7d3-5a27-43f6-b7e6-a737a4419406
+md"""
+## Part 2
+"""
+
+# ╔═╡ d9048bfa-f5b4-4257-b678-23755c97427e
+# puzzle part 2 data samples
+PDS_part2 = [
+	parse_puzzle_file("sample1.txt"),
+]
+
+# ╔═╡ 27fa11a9-657e-4382-9ed0-0a85df2da619
+fldmod(-200, 100)
+
+# ╔═╡ 97f36561-89ca-411c-9ac9-8b53e8b9b527
+function passed_zeros(start_num, final_num)
+	(start_group, start_rem) = fldmod(start_num, 100)
+	(final_group, final_rem) = fldmod(final_num, 100)
+	
+	correction = 0
+	if final_num < start_num
+		(start_rem == 0) && (correction += 1)
+		(final_rem == 0) && (correction -= 1)
+	end
+	
+	return abs(final_group - start_group + correction)
+end
+
+# ╔═╡ 38cef4ac-b455-4c2e-8a38-eea141b9fba4
+[
+	passed_zeros(45, 104),
+	passed_zeros(105, 45),
+	passed_zeros(-45, -104),
+	passed_zeros(-105, -45),
+]
+
+# ╔═╡ 14aa74e6-98e3-40b7-abdc-b5fb720a45d4
+[
+	passed_zeros(-245, 0),
+	passed_zeros(-145, 0),
+	passed_zeros(-45, 0),
+	passed_zeros(45, 0),
+	passed_zeros(145, 0),
+	passed_zeros(245, 0),
+]
+
+# ╔═╡ 5c9f0c17-aae6-4caa-b10c-e5b59c36791f
+[
+	passed_zeros(100, -100),
+	passed_zeros(-100, 100),
+]
+
+# ╔═╡ 89443d7e-1dc7-48af-ae76-543556ad4513
+[
+	passed_zeros(0, -245),
+	passed_zeros(0, -145),
+	passed_zeros(0, -45),
+	passed_zeros(0, 45),
+	passed_zeros(0, 145),
+	passed_zeros(0, 245),
+]
+
+# ╔═╡ e6938500-494f-4c3a-92e0-ba48fa360ec0
+function result2(pd)
+	x = 50
+	zero_counter = 0
+    for (; dir, mag) in pd
+		new_x = (dir == :R) ? x + mag : x - mag
+		zero_counter += passed_zeros(x, new_x)
+		x = new_x
+	end
+	return zero_counter
+end
+
+# ╔═╡ b5d4cd00-4d5b-4ab5-8771-23e471badb57
+result2.(PDS_part2)
+
+# ╔═╡ d496419e-572a-4fe7-9468-bd4d0be9e0c7
+md"""
+### answer
+"""
+
+# ╔═╡ 6cb8a5e4-d822-425e-ad9e-aeff375ad9ac
+@time @info(
+    "part 2 answer",
+    result2(PDI),
+)
+
+# ╔═╡ c366ad51-46da-47b4-af45-2d2374521857
+	md"""
+	## Standard helpers
+	"""
+
+# ╔═╡ 3460a9d6-dec6-4791-bbbb-f081e253f7d9
+import AdventOfCode:
+	AdventOfCode,
+	CI, CIS,
+	make_smallest_boundary,
+	parse_matrix,
+	print_matrix
+
+# ╔═╡ b405167e-613f-49ae-ad3d-30354d2c30a3
+begin
+	Base.show(io::IO, ::MIME"text/plain", c::CI) = print(io, "CI(", join(string.(Tuple(c)), ", "), ")")
+	Base.show(io::IO, c::CI) = show(io, "text/plain", c)
+	
+	Base.show(io::IO, ::MIME"text/plain", c::CIS) = print(io, "CIS((", join(c.indices, ", "), "))")
+	Base.show(io::IO, c::CIS) = show(io, "text/plain", c)
+	
+	Base.show(io::IO, ::MIME"text/plain", c::Char) = print(io, string(c))
+	
+	# Base.show(io::IO, ::MIME"text/plain", v::Vector) = print(io, "[", join(v, ", "), "]")
+	Base.show(io::IO, v::Vector) = print(io, "[", join(v, ", "), "]")
+end
+
+# ╔═╡ edb226f3-ee5c-4c18-85d5-daf795014160
+html"""
+<style>
+    main {
+        max-width: none; /* Removes the max-width constraint */
+        margin: 0 auto; /* Centers the content if there's extra space */
+        padding-left: 10%; /* Optional: add some padding on the sides */
+        padding-right: 10%;
+    }
+</style>
+"""
+
+# ╔═╡ Cell order:
+# ╟─213c3a51-ce8d-4c6f-811e-7f551fd2c356
+# ╟─bfd79601-6001-4737-bc9f-f13e02150c3c
+# ╟─ccd4eb29-5eb4-469c-99d7-1cdcd679fb66
+# ╟─d2638404-b96d-4362-850f-98fd5dabd5be
+# ╠═da7e182a-6c9d-4c43-91d4-b32be2545718
+# ╠═afe15c3c-0a5f-4db4-8f72-bd22b0b7e27b
+# ╠═d97c6185-39c1-4d26-b2d2-66dd000c317b
+# ╟─5436ac6a-5869-4b8d-9b9e-56e731589cbd
+# ╠═e9fc4134-09f9-43b8-8066-24ab50a2f64c
+# ╠═4c4f78ee-b679-4597-9a0e-a01a2060f231
+# ╠═5bdfe21f-9535-47f3-b162-5c63e49022ac
+# ╟─451ff7c6-1e2f-4717-9c15-92760473609c
+# ╠═0afa794b-17ef-4e24-a5de-caf2d4ff966e
+# ╟─2bbaf7d3-5a27-43f6-b7e6-a737a4419406
+# ╠═d9048bfa-f5b4-4257-b678-23755c97427e
+# ╠═27fa11a9-657e-4382-9ed0-0a85df2da619
+# ╠═97f36561-89ca-411c-9ac9-8b53e8b9b527
+# ╠═38cef4ac-b455-4c2e-8a38-eea141b9fba4
+# ╠═14aa74e6-98e3-40b7-abdc-b5fb720a45d4
+# ╠═5c9f0c17-aae6-4caa-b10c-e5b59c36791f
+# ╠═89443d7e-1dc7-48af-ae76-543556ad4513
+# ╠═e6938500-494f-4c3a-92e0-ba48fa360ec0
+# ╠═b5d4cd00-4d5b-4ab5-8771-23e471badb57
+# ╟─d496419e-572a-4fe7-9468-bd4d0be9e0c7
+# ╠═6cb8a5e4-d822-425e-ad9e-aeff375ad9ac
+# ╟─c366ad51-46da-47b4-af45-2d2374521857
+# ╠═090b4c81-9cda-42e4-a9ff-22fbf5844fa0
+# ╠═90815a37-6e8e-4c01-9e22-52dd96ab457b
+# ╠═56736de4-cc48-11f0-a6b8-e199bea039a2
+# ╠═3460a9d6-dec6-4791-bbbb-f081e253f7d9
+# ╠═b405167e-613f-49ae-ad3d-30354d2c30a3
+# ╠═edb226f3-ee5c-4c18-85d5-daf795014160
